26ed5e4370b8ad19d23dc348cc9bc907bf3c1857
spookylex <77869197+spookylex@users.noreply.github.com>
2022-06-18 23:06:01 -0400

    popups working & new templates

--- lyricsoftheworld/lotwapp/urls.py
+++ lyricsoftheworld/lotwapp/urls.py
@@ -3,4 +3,11 @@ from . import views
 
 urlpatterns = [
     path('', views.homepage, name='homepage'),
+    path('topfive', views.topfive, name='topfive'),
+    path('northamerica', views.northamerica, name='northamerica'),
+    path('southamerica', views.southamerica, name='southamerica'),
+    path('africa', views.africa, name='africa'),
+    path('europe', views.europe, name='europe'),
+    path('asia', views.asia, name='asia'),
+    path('australia', views.australia, name='australia'),
 ]
--- lyricsoftheworld/lotwapp/views.py
+++ lyricsoftheworld/lotwapp/views.py
@@ -3,3 +3,24 @@ from django.shortcuts import render, redirect
 # Create your views here.
 def homepage(request):
     return render(request, 'homepage.html')
+
+def topfive(request):
+    return render(request, 'topfive.html')
+
+def northamerica(request):
+    return render(request, 'northamerica.html')
+
+def southamerica(request):
+    return render(request, 'southamerica.html')
+
+def africa(request):
+    return render(request, 'africa.html')
+
+def europe(request):
+    return render(request, 'europe.html')
+
+def asia(request):
+    return render(request, 'asia.html')
+
+def australia(request):
+    return render(request, 'australia.html')
